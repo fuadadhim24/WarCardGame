cd0786bb106fa8457e2e4b60804c28a678450e85
fuadadhim24 <fuadadhim24@gmail.com>
2025-08-05 09:32:49 +0700

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Fuad Adhim Al Hasan on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
