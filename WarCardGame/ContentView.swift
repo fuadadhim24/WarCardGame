9a1e3f129c5ad83d3770ca802570b2ac21641346
fuadadhim24 <fuadadhim24@gmail.com>
2025-08-05 10:04:50 +0700

    feat: add fe main page

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -9,13 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/, count: 3, span: 1, spacing: 0)
+                Spacer()
+                    .frame(height: 40)
+                HStack{
+                    Image("card2")
+                    Spacer()
+                        .frame(width: 60)
+                    Image("card3")
+                }
+                Spacer()
+                    .frame(height: 40)
+                Image("button")
+                Spacer()
+                    .frame(height: 50)
+                HStack{
+                    VStack{
+                        Text("Player")
+                            .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                        Spacer()
+                            .frame(height: 14)
+                        Text("0")
+                            .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                        .frame(width: 60)
+                    VStack{
+                        Text("CPU")
+                            .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                        Spacer()
+                            .frame(height: 14)
+                        Text("0")
+                            .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
